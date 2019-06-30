3576847a7daea1e3ce52b569b99a53be98cd1149
KaiYi <oschangkai@gmail.com>
2019-06-30 21:57:53 +0800

    Delete setup-system.sh
    Remove old Files

--- setup-system.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update -y
-sudo apt upgrade -y
-
-sudo apt install -y --no-install-recommends git
-git clone https://github.com/nicokaiser/rpi-audio-receiver.git
-cd rpi-audio-receiver
-
-sudo raspi-config nonint do_hostname kaiyi-wirelessstereo
-sudo hostnamectl set-hostname --pretty "KaiYi-WirelessStereo"
